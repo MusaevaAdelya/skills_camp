625eed7021c9278af52bf2e15dd23f5542ea73e6
Adelya <msvadelya@gmail.com>
2023-06-13 10:05:31 +0600

    project created

---
+++ src/main/java/com/example/skills_camp/SkillsCampApplication.java
@@ -0,0 +1,13 @@
+package com.example.skills_camp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillsCampApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkillsCampApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/skills_camp/SkillsCampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.skills_camp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillsCampApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
